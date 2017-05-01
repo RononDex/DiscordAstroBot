9d72fc31ac9b83b049d1384c9d8f706cad6da441
RononDex <tinoheuberger@hotmail.com>
2017-05-01 16:47:04 +0200

    Continued work on customizable bot, adding admin commands

---
+++ DiscordAstroBot/DiscordAstroBot/Commands/AdminCommands.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Discord;
+
+namespace DiscordAstroBot.Commands
+{
+    /// <summary>
+    /// Controls at who the bot is mad at
+    /// </summary>
+    public class AdminCommands : Command
+    {
+        public override string[] CommandSynonyms { get; }
+
+        public override bool MessageRecieved(Match matchedMessage, MessageEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DiscordAstroBot/DiscordAstroBot/XmlSerialization/XmlStateController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DiscordAstroBot.XmlSerialization
+{
+    /// <summary>
+    /// Allows to load and save xml files
+    /// </summary>
+    public static class XmlStateController
+    {
+        public static T LoadObject<T>(string xmlFile)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            return (T)serializer.Deserialize(new StringReader(File.ReadAllText(xmlFile)));
+        }
+
+        public static void SaveObject<T>(T obj, string xmlFile)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            serializer.Serialize(new FileStream(xmlFile, FileMode.OpenOrCreate), obj);
+        }
+    }
+}
