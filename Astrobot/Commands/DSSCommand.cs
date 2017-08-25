f54ea0badb824b6995ef4cb91a70f27c4939c92e
RononDex <tinoheuberger@hotmail.com>
2017-08-25 15:17:16 +0200

    DSS command now allows for names with space in it

--- Astrobot/Commands/DSSCommand.cs
+++ Astrobot/Commands/DSSCommand.cs
@@ -18,7 +18,7 @@ namespace DiscordAstroBot.Commands
     {
         public override string CommandName => "DSS";
 
-        public override string[] CommandSynonyms => new string[] {@"(how|what) does (?'ObjectName'\w*) look like(\?)?"};
+        public override string[] CommandSynonyms => new string[] { @"(how|what) does (?'ObjectName'.+?(?= look)) look like(\?)?" };
 
         public override string Description => "Query DSS database for how some deep space object looks like. Usage:\r\n```    @Astro Bot How does M63 look like```";
 
