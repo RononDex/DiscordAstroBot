cd493154c4f0c479e77dcccc0a8c654fe57a4043
RononDex <tinoheuberger@hotmail.com>
2017-08-25 11:26:14 +0200

    First version of Help command

--- Astrobot/Commands/Help.cs
+++ Astrobot/Commands/Help.cs
@@ -9,17 +9,26 @@ using Discord.WebSocket;
 
 namespace DiscordAstroBot.Commands
 {
+    /// <summary>
+    /// Command to display help on how to use the bot
+    /// </summary>
     public class Help : Command
     {
         public override string CommandName => "Help";
 
-        public override string[] CommandSynonyms => base.CommandSynonyms;
+        public override string[] CommandSynonyms => new [] { "help" };
 
         public override string Description => "Displays this help";
 
         public override async Task<bool> MessageRecieved(Match matchedMessage, SocketMessage recievedMessage)
         {
-            return false;
+            var output =
+                Mappers.Config.ServerCommands.Config.CommandsConfigServer.Single(
+                    x => x.ServerID == ((SocketGuildChannel) recievedMessage.Channel).Guild.Id)
+                    .Commands.Where(x => x.Enabled)
+                    .Select(x => $"**{x.CommandName}**: {Utilities.CommandsUtility.ResolveCommand(x.CommandName).Description} \r\n");
+            await recievedMessage.Channel.SendMessageAsync($"Available commands on this server:\r\n{output}");
+            return true;
         }
     }
 }
